c2637ca2a7f7dc3ab4b05430c4a70a14d1af3f46
MuhammadHuzaifaSheikh <huzaifa900shaikh@gmail.com>
2021-07-01 09:57:22 -0700

    pdf done

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 
+let input = document.querySelector('input');
+let button = document.querySelector('.button')
+ let pdfBtn =      button.firstElementChild;
+pdfBtn.innerHTML="PDF";
+let pptBtn = pdfBtn.nextElementSibling;
+pptBtn.innerHTML= 'PPT';
 
-
-
-
+let images = [];
 function getHtml() {
+    pdfBtn.innerHTML= `
+     <div class="spinner-border text-success" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+    `
+    pptBtn.innerHTML= `
+     <div class="spinner-border text-success" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+    `
+
+    images= []
     var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
+        method: 'POST',
+        body: JSON.stringify({siteUrl:input.value}),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
     };
 
     fetch("http://localhost:5000/sendLink", requestOptions)
         .then(response => response.text())
         .then(result => {
-            let xmlString = result;
-            let htmlEle = new DOMParser().parseFromString(xmlString, "text/html");
-            let slide_container =  htmlEle.body.querySelector('.slide_container')
-            getAllSlides(slide_container)
+            let htmlString = result;
+            let htmlEle = new DOMParser().parseFromString(htmlString, "text/html");
+            let slide_container =  htmlEle.body.querySelectorAll('.slide_container>section>img')
+          if (slide_container.length>0){
+              getAllSlides(slide_container)
+          }
+          else {
+              alert('Cannot download the file');
+              pptBtn.innerHTML= 'PPT';
+              pdfBtn.innerHTML= 'PDF';
+
+          }
         })
         .catch(error => console.log('error', error));
 
@@ -23,12 +51,28 @@ function getHtml() {
 
 
 function getAllSlides(slide_container) {
-    // let images = [];
-    for (let i = 0 ; i<slide_container.childElementCount;i++){
-        let a = slide_container.querySelector('section').getAttribute("data-index")
-        console.log(a);
+    slide_container.forEach((image)=>{
+        images.push(image.src)
+    })
+    PdfGenerator(images)
+    pptBtn.innerHTML= 'PPT';
+    pdfBtn.innerHTML= 'PDF';
+}
 
+
+function PdfGenerator(images) {
+    printJS({
+        printable: images,
+        type: 'image',
+        showModal: true, // Optional
+        modalMessage: 'Printing Images...', // Optional
+        style: ['@page { size: A4; margin: 0mm;} body {margin-top: 0;}  img { width:100%}'],
+        targetStyles: ['*']
     }
-    // console.log(slide_container);
+    );
 
 }
+
+
+
+
